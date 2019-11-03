fc97141c32ef747de7e8335b72a2cf438f844ce4
metherton <martinetherton63@gmail.com>
2019-11-03 22:26:33 +0100

    Add methods for Queue

--- src/main/scala/com/martinetherton/trainscheduler/misc/Queue.scala
+++ src/main/scala/com/martinetherton/trainscheduler/misc/Queue.scala
@@ -1,6 +1,14 @@
 package com.martinetherton.trainscheduler.misc
 
-  class Queue[A](val elements: Seq[A]) {
+object Queue {
+
+  def apply[A](elements: Seq[A]): Queue[A] = {
+    new Queue(elements)
+  }
+
+}
+
+class Queue[A] private (val elements: Seq[A]) {
 
   override def equals(o: Any): Boolean = o match {
     case that: A => that == o
@@ -12,4 +20,9 @@ package com.martinetherton.trainscheduler.misc
   override def toString(): String = "elements are " + elements
 
     //f"$hours%02d:$minutes%02d"
+
+  def dequeue(): (A, Queue[A]) = elements match {
+    case Nil => throw new UnsupportedOperationException
+    case head :: tail => (head ,new Queue(tail))
+  }
 }
--- src/test/scala/com/martinetherton/trainscheduler/misc/QueueTest.scala
+++ src/test/scala/com/martinetherton/trainscheduler/misc/QueueTest.scala
@@ -6,22 +6,32 @@ class QueueTest extends WordSpec with Matchers {
 
   "equals" should {
     "equal passed in" in {
-      val intQueue = new Queue(List(1,2,3))
-      intQueue.equals(new Queue(List(1,2,3))) shouldBe true
+      val intQueue = Queue(List(1,2,3))
+      intQueue.equals(Queue(List(1,2,3))) shouldBe true
     }
   }
 
   "hashCode" should {
     "hashCode passed in" in {
-      val intQueue = new Queue(List(1,2,3))
+      val intQueue = Queue.apply(List(1,2,3))
       intQueue.hashCode() shouldBe 41
     }
   }
 
   "toString" should {
     "toString passed in" in {
-      val intQueue = new Queue(List(1,2,3))
+      val intQueue = Queue(List(1,2,3))
       intQueue.toString() shouldBe "elements are List(1, 2, 3)"
+    }
+  }
+
+  "dequeue" should {
+    "return first and another queue" in {
+      val intQueue = Queue(List(1,2,3))
+      val newQueue = intQueue.dequeue()
+      newQueue._1 shouldBe 1
+      newQueue._2 shouldBe Queue(List(2,3))
+
     }
   }
 
